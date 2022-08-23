b0c8ecb12a1d7d0b3273c91a8ee3e44e2311e0bb
eMu <emad.easa@codecool.com>
2022-08-23 05:09:24 +0200

    added new projects

---
+++ Projects/DescribeYourself/describe.js
@@ -0,0 +1,12 @@
+// variables are declared with "let" or "var"
+// btw. this is a comment - with comments I can explain my code to remember later, what the code does
+let lastname=""; // this is a variable of datatype string
+let firstname="";
+let height=0; // in cm - this is a variable from datatype int
+let weight=0; // in kg
+let isFemale=true; // this is a variable of datatype boolean - it is true if you are female
+
+console.log("Hello " + firstname + " " + lastname);
+console.log("I know some things about you!");
+console.log("You are " + height + " cm tall.");
+console.log("Your weight is " + weight + " kg.");
