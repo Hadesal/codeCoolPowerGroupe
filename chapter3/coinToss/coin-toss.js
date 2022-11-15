4ea7918200111cb0a286a11a4125ce1ae6b51ba7
Hadesal <hadealhlo1977@gmail.com>
2022-11-15 20:09:04 +0100

    files conflict

---
+++ chapter3/coinToss/coin-toss.js
@@ -0,0 +1,16 @@
+function main() {
+  let coinHeads = document.getElementById("coin-heads");
+  let coinTails = document.getElementById("coin-tails");
+  let message = document.getElementById("message");
+  const images = [coinHeads, coinTails];
+  const texts = ["You have chosen heads", "You have chosen tails"];
+  coinHeads.style.display = "none";
+  coinTails.style.display = "none";
+
+  window.onload = function () {
+    let x = Math.floor(Math.random() * 2);
+    images[x].style.display = "flex";
+    message.innerHTML = texts[x];
+  };
+}
+main();
